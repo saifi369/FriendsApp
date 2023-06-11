299b23d2b3b4743ffa91233ae6f4f2a6d919ec5f
M-Saif-Ullah <chsaif369@gmail.com>
2023-06-11 22:03:17 +0500

    Failing Signup Test

--- app/src/androidTest/java/com/u4universe/friendsapp/signup/SignUpRobot.kt
+++ app/src/androidTest/java/com/u4universe/friendsapp/signup/SignUpRobot.kt
@@ -1,4 +1,53 @@
 package com.u4universe.friendsapp.signup
 
-class SignUpRobot {
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.u4universe.friendsapp.MainActivity
+import com.u4universe.friendsapp.R
+
+fun launchSignUpScreen(
+  rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>,
+  block: SignUpRobot.() -> Unit
+): SignUpRobot {
+  return SignUpRobot(rule).apply(block)
+}
+
+class SignUpRobot(
+  private val rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>
+) {
+  fun typeEmail(email: String) {
+    val emailHintText = rule.activity.getString(R.string.emailHint)
+    rule.onNodeWithText(emailHintText)
+      .performTextInput(email)
+  }
+
+  fun typePassword(password: String) {
+    val passwordHintText = rule.activity.getString(R.string.passwordHint)
+    rule.onNodeWithText(passwordHintText)
+      .performTextInput(password)
+  }
+
+  fun submit() {
+    val submitText = rule.activity.getString(R.string.submitButtonText)
+    rule.onNodeWithText(submitText)
+      .performClick()
+  }
+
+  infix fun verify(block: SignUpVerification.() -> Unit): SignUpVerification {
+    return SignUpVerification(rule).apply(block)
+  }
+
+}
+
+class SignUpVerification(private val rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>) {
+  fun timelineScreenIsPresent() {
+    val timeline = rule.activity.getString(R.string.timeline)
+    rule.onNodeWithText(timeline)
+      .assertIsDisplayed()
+  }
+
 }
--- app/src/androidTest/java/com/u4universe/friendsapp/signup/SignUpTest.kt
+++ app/src/androidTest/java/com/u4universe/friendsapp/signup/SignUpTest.kt
@@ -1,4 +1,23 @@
 package com.u4universe.friendsapp.signup
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.u4universe.friendsapp.MainActivity
+import org.junit.Rule
+import org.junit.Test
+
 class SignUpTest {
+
+  @get:Rule
+  val signUpTestRule = createAndroidComposeRule<MainActivity>()
+
+  @Test
+  fun performSignUp() {
+    launchSignUpScreen(signUpTestRule) {
+      typeEmail("test@email.com")
+      typePassword("password")
+      submit()
+    } verify {
+      timelineScreenIsPresent()
+    }
+  }
 }
