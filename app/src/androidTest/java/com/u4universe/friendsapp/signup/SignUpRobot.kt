5d4a033db6b6d970fc191cce89d350705b81c629
M Saif Ullah <chsaif369@gmail.com>
2023-06-17 03:17:06 +0500

    added testTag for Password Field

--- app/src/androidTest/java/com/u4universe/friendsapp/signup/SignUpRobot.kt
+++ app/src/androidTest/java/com/u4universe/friendsapp/signup/SignUpRobot.kt
@@ -2,6 +2,7 @@ package com.u4universe.friendsapp.signup
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
@@ -10,44 +11,44 @@ import com.u4universe.friendsapp.MainActivity
 import com.u4universe.friendsapp.R
 
 fun launchSignUpScreen(
-  rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>,
-  block: SignUpRobot.() -> Unit
+    rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>,
+    block: SignUpRobot.() -> Unit
 ): SignUpRobot {
-  return SignUpRobot(rule).apply(block)
+    return SignUpRobot(rule).apply(block)
 }
 
 class SignUpRobot(
-  private val rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>
+    private val rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>
 ) {
-  fun typeEmail(email: String) {
-    val emailHintText = rule.activity.getString(R.string.emailHint)
-    rule.onNodeWithText(emailHintText)
-      .performTextInput(email)
-  }
+    fun typeEmail(email: String) {
+        val emailHintText = rule.activity.getString(R.string.emailHint)
+        rule.onNodeWithText(emailHintText)
+            .performTextInput(email)
+    }
 
-  fun typePassword(password: String) {
-    val passwordHintText = rule.activity.getString(R.string.passwordHint)
-    rule.onNodeWithText(passwordHintText)
-      .performTextInput(password)
-  }
+    fun typePassword(password: String) {
+        val passwordHintText = rule.activity.getString(R.string.passwordHint)
+        rule.onNodeWithTag(passwordHintText)
+            .performTextInput(password)
+    }
 
-  fun submit() {
-    val submitText = rule.activity.getString(R.string.submitButtonText)
-    rule.onNodeWithText(submitText)
-      .performClick()
-  }
+    fun submit() {
+        val submitText = rule.activity.getString(R.string.submitButtonText)
+        rule.onNodeWithText(submitText)
+            .performClick()
+    }
 
-  infix fun verify(block: SignUpVerification.() -> Unit): SignUpVerification {
-    return SignUpVerification(rule).apply(block)
-  }
+    infix fun verify(block: SignUpVerification.() -> Unit): SignUpVerification {
+        return SignUpVerification(rule).apply(block)
+    }
 
 }
 
 class SignUpVerification(private val rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>) {
-  fun timelineScreenIsPresent() {
-    val timeline = rule.activity.getString(R.string.timeline)
-    rule.onNodeWithText(timeline)
-      .assertIsDisplayed()
-  }
+    fun timelineScreenIsPresent() {
+        val timeline = rule.activity.getString(R.string.timeline)
+        rule.onNodeWithText(timeline)
+            .assertIsDisplayed()
+    }
 
 }
--- app/src/main/java/com/u4universe/friendsapp/signup/SignUpScreen.kt
+++ app/src/main/java/com/u4universe/friendsapp/signup/SignUpScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -88,7 +89,10 @@ private fun PasswordField(
         mutableStateOf(false)
     }
     TextField(
-        modifier = Modifier.fillMaxWidth(), trailingIcon = {
+        modifier = Modifier
+            .fillMaxWidth()
+            .testTag(stringResource(id = R.string.passwordHint)),
+        trailingIcon = {
             IconButton(onClick = {
                 isVisible = !isVisible
             }) {
